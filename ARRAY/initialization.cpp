9195d3bb739cccd672d180b328662a77a5d6d9ef
khushi singhal <khushisinghal201120@gmail.com>
2024-04-08 20:58:30 +0530

    ARRAY ramesh

---
+++ ARRAY/initialization.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std ;
+int main()
+{
+    int A[5]  ;
+    cout << "enter values" <<endl ;
+    for(int i=0;i<5;i++)
+         cin>>A[i] ;
+     for(int i=0;i<5;i++)
+        cout<<"A["<<i<<"] = "<<A[i]<<endl ;
+}
