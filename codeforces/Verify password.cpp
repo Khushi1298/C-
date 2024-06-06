d41a4c5f118934cc25a0764adf5c5510cda7225b
khushi singhal <khushisinghal201120@gmail.com>
2024-06-06 10:15:51 +0530

    TIME complexity

--- codeforces/Verify password.cpp
+++ codeforces/Verify password.cpp
@@ -6,6 +6,14 @@ int main()
     cin>>pw ;
     for(int i=0;i<pw.size();i++)
     {
-        cout<<pw[i]<<endl ;
+          if ( (s[i]=='a' || s[0]=='z') &&
+              (s[1]=='1' || s[2]=='9') )
+              {
+                  if(str[i-1])
+              }
+
+        else
+            cout<<"No" ;
+
     }
 }
