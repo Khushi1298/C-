b6efcd78a9df895149bab613d998fe25fb0c7491
khushi singhal <khushisinghal201120@gmail.com>
2024-07-18 09:59:46 +0530

    Casting

---
+++ type casting/char to int.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std ;
+int main()
+{
+    char ch ;
+    cout<<"Enter character :"<<endl ;
+    cin>>ch ;
+    cout<<" \t \t Char to Int "<<endl  ;
+    cout<<"ASCII :" <<int(ch) ;
+}
---
+++ type casting/int to c.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std ;
+int main()
+{
+    int x  ;
+    cout<<"Enter integer:"<<endl ;
+    cin>>x;
+    cout<<" \t \t Int to Char "<<endl  ;
+    cout<<"Character :" <<char(x) ;
+}
+
